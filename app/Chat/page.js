8501d2acfedde0b48fb3df255d9118391fe8e1d2
Kadir <Kadir@DESKTOP-P6D4VNP>
2025-01-29 21:34:52 +0300

    ChatPage ayarlanıyor

--- app/Chat/page.js
+++ app/Chat/page.js
@@ -1,18 +1,28 @@
 "use client";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { getSession } from "next-auth/react";
-
-
+import { Grid2 as Grid, TextField, Button } from '@mui/material';
+import styles from "./styles.module.css";
 
 export default function Page() {
-  const session =getSession();
-  useEffect(()=>{
-    session.then(session=>{
-      console.log(session);
+  const session = getSession();
+  
+  useEffect(() => {
+    session.then(session => {
+      if (session) {
+        console.log(session);
+      } else {
+        location.href = "/";
+      }
     });
-  },[]);
+  }, []);
 
-  return <div>Chat Sayfası</div>;
-
+  return (
+    <Grid container height="100vh" sx={{ backgroundColor: '#f5f5f5' }}>
+      <Grid 
+        xl={2} borderRadius={2} className={styles.leftPanelGrid}   
+      ></Grid>
+    </Grid>
+  );
 }
-
